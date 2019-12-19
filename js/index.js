ee69cab2d52e5e0f12316e782952cdc36fc5a733
mutazalaa <mutazalaasalah@gmail.com>
2019-12-19 21:27:27 +0200

    edit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+class App{
+    constructor(title , date){
+        this.title;
+        this.date;
+    }
+}
+
 class UI{
     createElement(tagname,className,content){
        let item= document.createElement(tagname);
@@ -8,7 +15,7 @@ class UI{
     createEvent(){
         let AllEvent = this.createElement('div' , 'allevent')
         let titleEvent = this.createElement('h1' , 'title',title.value)
-        let EventDays = this.createElement('h3','days')
+        let EventDays = this.createElement('h3','days' , `D`)
         let EventHours = this.createElement('h3','hours')
         let Eventmins = this.createElement('h3','mins')
         let Eventsecs = this.createElement('h3','secs')
@@ -25,16 +32,22 @@ class UI{
         document.querySelector('.event').appendChild(AllEvent)
        
     }
-    
-     distanceTime(){
+    distanceTime(){
         setInterval(() => {
             let now = new Date().getTime();
             let countDown = new Date(date.value).getTime();
             let distance = countDown - now ; 
-            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+           //trying to make destructuring -_-
+            let time =
+            [
+                Math.floor(distance / (1000 * 60 * 60 * 24)),
+                Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+                Math.floor((distance % (1000 * 60)) / 1000)
+                
+            ]
+            let [days , hours ,minutes,seconds] = time;
+           
             // document.querySelector('.event').innerHTML =`<h3>${days} D</h3><h3>${hours} H</h3><h3>${minutes} D</h3><h3>${seconds} S</h3> ` 
             
             if(distance <0){
@@ -44,7 +57,7 @@ class UI{
                 document.querySelector('.secs').innerHTML=`ED`    
               }
             else{
-                document.querySelector('.days').innerHTML=days +`D`
+                // document.querySelector('.days').innerHTML=days +`D`
                 document.querySelector('.hours').innerHTML=hours +`H`
                 document.querySelector('.mins').innerHTML=minutes +`M`
                 document.querySelector('.secs').innerHTML=seconds +`S`
@@ -60,7 +73,7 @@ class UI{
     validForm(){
         document.querySelector('button').addEventListener('click',()=>{
         if(title.value === "" && date.value ===""){
-            window.alert('No item Yet')
+            window.alert('Fill is empty')
         }
         else{  
             ui.createEvent();
@@ -73,12 +86,33 @@ ui.resetForm();
 ui.validForm();
 const title = document.querySelector('.title'); 
 const date =document.querySelector('.date') ;
+
+
+
+
+
+
+
+
+
+
+
 // let title=document.querySelector('.title');
 // let date =document.querySelector('.date');
 // let form = document.querySelector('form');
 // form.addEventListener('submit',(e)=>{
 //     e.preventDefault();
 // })
+
+
+
+
+
+
+
+
+
+
 
 //     document.querySelector('button').addEventListener('click',()=>{
 //         setInterval(() => {
